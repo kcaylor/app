43f25a1049e126a0756af9a22dd69b1c8a17c50f
Kelly Caylor <kelly@pulsepod.io>
2014-09-03 22:14:25 -0400

    Something special for Ben :)

--- app/main/views.py
+++ app/main/views.py
@@ -7,7 +7,8 @@ from app.shared.models.sensor import Sensor
 from app.shared.models.notebook import Notebook
 from app.shared.models.user import User
 from app.shared.models.pod import Pod
-
+from app.shared.models.message import Message
+from app.decorators import admin_required
 from mongoengine import Q
 
 
@@ -16,6 +17,7 @@ from mongoengine import Q
 #     if not current_user.is_authenticated():
 #         return redirect(url_for('auth.login'))
 NBK_PER_PAGE = 5
+MSG_PER_PAGE = 5
 
 
 @main.route('/')
@@ -40,6 +42,31 @@ def index():
         )
         flash(message, 'warning')
     return render_template('index.html')
+
+
+@main.route('/messages')
+@main.route('/messages/<int:page>')
+@login_required
+@admin_required
+def messages(page=1):
+    messages = Message.objects().order_by('-time_stamp').paginate(
+        page=page, per_page=MSG_PER_PAGE
+    )
+    queued_messages = Message.objects(
+        status='queued'
+    ).order_by('-time_stamp')
+    for message in queued_messages:
+        url = url_for('main.message_info', _id=message.get_id())
+        alert = Markup(
+            "Warning: Message <a href=%s>%s</a> is queued \
+            and has not been processed." % (url, message.message_id)
+        )
+        flash(alert, 'warning')
+    return render_template(
+        'main/message_list.html',
+        current_time=datetime.utcnow(),
+        messages=messages
+    )
 
 
 @main.route('/notebooks')
@@ -126,30 +153,39 @@ def user(username):
     )
 
 
+@main.route('/message/<_id>')
+@login_required
+@admin_required
+def message_info(_id):
+    message = Message.objects(id=_id).first()
+    return render_template(
+        'main/message_info.html',
+        current_time=datetime.utcnow(),
+        message=message
+    )
+
+
 @main.route('/notebook/<_id>')
 @login_required
 def notebook_info(_id):
     notebook = Notebook.objects(
         id=_id
     ).first()
+    # Should really do this as an AJAX:
     notebook.xls()
     data = Data.objects(
         notebook=notebook
-    ).order_by(
-        '-time_stamp'
-    ).only(
-        'time_stamp', 'value', 'variable'
-    ).limit(100)
+    ).first()
     sensors = Sensor.objects(
         sid__in=notebook.sids
     )
-    if len(data) is 0:
+    if not data:
         flash('Waiting for initial data transmission', 'warning')
     return render_template(
         'notebook_info.html',
         current_time=datetime.utcnow(),
         notebook=notebook,
-        data=data,
+        # data=data,  # No need to return data, because AJAX.
         sensors=sensors,
     )
 
