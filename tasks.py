94071ea4e1f93ccd74fb80b642457ebc2e3445f4
Kelly Caylor <kelly@pulsepod.io>
2016-03-27 12:38:45 +0300

    fixed acl statement on S3

--- tasks.py
+++ tasks.py
@@ -27,12 +27,12 @@ def create_xls_notebook(self, nbk_id=None):
         # Create this key.
         key = bucket.new_key(s3_filename)
         # key.set_contents_from_string('temp')  # need something to make URL.
-        bucket.set_acl('public-read', s3_filename)
         # Build the notebook:
         from app.shared.models.notebook import Notebook
         notebook = Notebook.objects(id=nbk_id).first()
         tmp_file = notebook.xls()
         key.set_contents_from_filename(tmp_file)
         url = key.generate_url(expires_in=0, query_auth=False)
+        bucket.set_acl('public-read', s3_filename)
         self.update_state(state='PROGESS', meta={'url': url, 'nbk_id': nbk_id})
         return {'status': 'Task completed!', 'url': url}
